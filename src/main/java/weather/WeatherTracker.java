6fadd1f4ae834e607c0c51a0f76bb71d75dc0938
박시우 <pswudo@gmail.com>
2026-09-17 17:59:31 +0900

    refactor: apply DIP to Weather

--- src/main/java/weather/WeatherTracker.java
+++ src/main/java/weather/WeatherTracker.java
@@ -1,23 +1,27 @@
 package weather;
 
 public class WeatherTracker {
-    String currentConditions;
-    Phone phone;
-    Email email;
 
-    public WeatherTracker() {
-        phone = new Phone();
-        email = new Email();
+    String currentConditions;
+
+    private final Notifier rainyNotifier;
+    private final Notifier sunnyNotifier;
+
+    public WeatherTracker(Notifier rainyNotifier, Notifier sunnyNotifier) {
+        this.rainyNotifier = rainyNotifier;
+        this.sunnyNotifier = sunnyNotifier;
     }
 
     public void setCurrentConditions(String weatherDescription) {
         this.currentConditions = weatherDescription;
+
         if (weatherDescription == "rainy") {
-            String alert = phone.generateWeatherAlert(weatherDescription);
+            String alert = rainyNotifier.generateWeatherAlert(weatherDescription);
             System.out.print(alert);
         }
+
         if (weatherDescription == "sunny") {
-            String alert = email.generateWeatherAlert(weatherDescription);
+            String alert = sunnyNotifier.generateWeatherAlert(weatherDescription);
             System.out.print(alert);
         }
     }
