c9b68425d7af2c2712e232f1be59d0423168f6df
박시우 <pswudo@gmail.com>
2026-09-17 16:16:23 +0900

    refactor: apply OCP to Greeter

---
+++ src/main/java/greeter/IntimateGreeting.java
@@ -0,0 +1,9 @@
+package greeter;
+
+public class IntimateGreeting implements GreetingStrategy {
+
+    @Override
+    public String greet() {
+        return "Hello Darling!";
+    }
+}
--- src/test/java/greeter/GreeterTest.java
+++ src/test/java/greeter/GreeterTest.java
@@ -1,32 +1,36 @@
 package greeter;
 
-import greeter.Greeter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GreeterTest {
+
     @Test
     public void testSaysHello() {
         Greeter greeter = new Greeter();
+
         assertEquals("Hello.", greeter.greet());
     }
 
     @Test
     public void testSaysHelloFormally() {
-        Greeter greeter = new Greeter();
-        greeter.setFormality("formal");
+        Greeter greeter = new Greeter(new FormalGreeting());
+
         assertEquals("Good evening, sir.", greeter.greet());
     }
 
     @Test
     public void testSaysHelloCasually() {
-        Greeter greeter = new Greeter();
-        greeter.setFormality("casual");
+        Greeter greeter = new Greeter(new CasualGreeting());
+
         assertEquals("Sup bro?", greeter.greet());
     }
 
     @Test
     public void testSaysHelloIntimately() {
-        Greeter greeter = new Greeter();
-        greeter.setFormality("intimate");
+        Greeter greeter = new Greeter(new IntimateGreeting());
+
         assertEquals("Hello Darling!", greeter.greet());
     }
 }
